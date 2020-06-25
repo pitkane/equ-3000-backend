644f3383e87687278b801b831b27078007782c7e
Mikko Pitkänen <mikko@mbit.fi>
2020-06-25 19:26:03 +0300

    init

---
+++ index.ts
@@ -0,0 +1,40 @@
+import * as Hapi from "@hapi/hapi";
+
+const host = "localhost";
+const port = 8080;
+
+/** App */
+const server: Hapi.Server = new Hapi.Server({ host, port });
+
+/** Routes */
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) =>
+    "This is the GET route.",
+});
+
+server.route({
+  method: "POST",
+  path: "/",
+  handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+    return {
+      message:
+        "This is the POST route, the data you sent is attached to this response",
+      data: request.payload,
+    };
+  },
+});
+
+async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server running @ ${server.info.uri}`);
+}
+
+start();
